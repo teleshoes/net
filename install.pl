92be5a0230423eb1e568c6db97d4dafaf19e9236
Elliot Wolk <elliot.wolk@gmail.com>
2012-01-03 21:22:17 -0500

    removed unused setuid, especially since perl 5.12 doesnt have perl-suid

--- install.pl
+++ install.pl
@@ -8,9 +8,6 @@ if(`whoami` ne "root\n"){
   die "must be run as root; look at what i do first\n";
 }
 
-my @setuids = qw();
-my %isSetuid = map {$_ => 1} @setuids;
-
 sub run(@){
   print "exec: " . join(' ', @_) . "\n";
   system @_;
@@ -20,7 +17,4 @@ for my $x(`ls src`){
   chomp $x;
   run "rm $prefix/$x";
   run "cp src/$x $prefix";
-  if($isSetuid{$x}){
-    run "chmod +s $prefix/$x";
-  }
 }
