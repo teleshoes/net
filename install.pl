8bdd93adca3d02332ebb4509fe887d2b4199dcbb
Elliot Wolk <elliot.wolk@gmail.com>
2013-05-17 21:58:52 -0400

    cleanup net install, dont cp bash_completion to prefix

--- install.pl
+++ install.pl
@@ -5,22 +5,34 @@ use warnings;
 my $prefix = "/usr/local/bin";
 my $bcDir = "/etc/bash_completion.d";
 
-if(`whoami` ne "root\n"){
-  exec "sudo", $0, @ARGV;
-}
-
-sub run(@){
-  print "exec: " . join(' ', @_) . "\n";
+sub runOrDie(@){
+  run @_;
+  print "@_\n";
   system @_;
+  die "FAILED: @_\n" if $? != 0;
 }
 
-for my $x(`ls src`){
-  chomp $x;
-  run "rm $prefix/$x";
-  run "cp src/$x $prefix";
+sub main(@){
+  die "Usage: $0\n" if @_ > 0;
+
+  if(`whoami` ne "root\n"){
+    print "rerunning as root\n";
+    exec "sudo", $0, @ARGV;
+  }
+
+  my $dir = `dirname $0`;
+  chomp $dir;
+  chdir $dir;
+
+  for my $f(`ls src`){
+    chomp $f;
+    next if $f eq "bash_completion";
+    runOrDie "cp", "src/$f", "$prefix";
+  }
+
+  runOrDie "cp", "src/bash_completion", "$bcDir/net-ssids";
+  runOrDie "chown", "root.root", "$bcDir/net-ssids";
+  runOrDie "chmod", "644", "$bcDir/net-ssids";
 }
 
-run "cp src/bash_completion $bcDir/net-ssids";
-run "chown root.root $bcDir/net-ssids";
-run "chmod 644 $bcDir/net-ssids";
-
+&main(@ARGV);
